baf448633ce8ec12cfd613a41820c3627a5dc381
Ivan Rafi Susanto <susantoivan0611@gmail.com>
2021-12-29 15:50:16 +0700

    eslint

--- src/Pages/PKB/AddNewPKB.js
+++ src/Pages/PKB/AddNewPKB.js
@@ -1,154 +1,237 @@
-import { Typography, Box, Paper, Grid, Button, Modal, Fade, Backdrop, TextField, List, ListItem, ListItemText, InputAdornment  } from "@material-ui/core";
+import { Typography, Paper, Grid, Button, TextField, Box } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
-import { Search } from "@material-ui/icons";
-import { useState } from "react";
-import { useHistory, useParams } from "react-router";
+import { useState } from 'react';
+import { useHistory, useParams } from 'react-router';
 import axios from 'axios';
 import uniqid from 'uniqid';
 
-const useStyles = makeStyles((theme)=>({
+const useStyles = makeStyles((theme) => ({
   modal: {
     display: 'flex',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'center'
   },
   paper: {
     backgroundColor: theme.palette.background.paper,
     boxShadow: theme.shadows[5],
-    padding: theme.spacing(2, 4, 3),
+    padding: theme.spacing(2, 4, 3)
   },
-  boxInput:{
-    marginBlock:theme.spacing(3),
-    padding:theme.spacing(3),
+  boxInput: {
+    marginBlock: theme.spacing(3),
+    padding: theme.spacing(3)
   }
 }));
 
-function InputServiceForm(props){
-  const {id} = useParams();
+function InputServiceForm() {
+  const { id } = useParams();
+  const [inputData, setInputData] = useState({
+    name: '',
+    phone_no: '',
+    email: '',
+    address: ''
+  });
+  const history = useHistory();
+  const classes = useStyles();
 
-const [open, setOpen] = useState(false);
-const [inputData, setInputData] = useState({
-  name:'',
-  phone_no:'',
-  email:'',
-  address:'',
-})
-const history = useHistory()
-const classes = useStyles()
-
-async function onSubmit(event){
-  event.preventDefault()
-  axios.post('http://localhost:8000/service/mechanic',{
-    id: uniqid("mechanic-"),
-    name: inputData.name,
-    phone_no: inputData.phone_no,
-    email: inputData.email,
-    address: inputData.address
-  }).then(res => {
-    if(res.status === 201){
-      history.push({
-        pathname: '/mekanik',
-        state: "Success"
+  async function onSubmit(event) {
+    event.preventDefault();
+    axios
+      .post('http://localhost:8000/service/mechanic', {
+        id: uniqid('mechanic-'),
+        name: inputData.name,
+        phone_no: inputData.phone_no,
+        email: inputData.email,
+        address: inputData.address
       })
-    }else{
-      history.push({
-        pathname: '/mekanik',
-        state: "Failed"
+      .then((res) => {
+        if (res.status === 201) {
+          history.push({
+            pathname: '/mekanik',
+            state: 'Success'
+          });
+        } else {
+          history.push({
+            pathname: '/mekanik',
+            state: 'Failed'
+          });
+        }
+      });
+  }
+
+  return (
+    <Grid container direction="column" component={Paper} className={classes.boxInput}>
+      <Typography variant="h5" color="primary" style={{ marginBottom: 20 }}>
+        Input Service
+      </Typography>
+      <Box display="flex" style={{ marginBlock: 10 }}>
+        <TextField
+          onChange={(event) => {
+            setInputData({ ...inputData, name: event.target.value });
+            event.preventDefault();
+          }}
+          value={id}
+          label="Nama Mekanik"
+          variant="outlined"
+          size="small"
+          style={{ width: 450 }}
+        />
+      </Box>
+      <Box display="flex" style={{ marginBlock: 10 }}>
+        <TextField
+          onChange={(event) => {
+            setInputData({ ...inputData, phone_no: event.target.value });
+            event.preventDefault();
+          }}
+          value={inputData.phone_no}
+          label="Nomor Telepon"
+          variant="outlined"
+          size="small"
+          style={{ width: 450 }}
+        />
+      </Box>
+      <Box display="flex" style={{ marginBlock: 10 }}>
+        <TextField
+          onChange={(event) => {
+            setInputData({ ...inputData, email: event.target.value });
+            event.preventDefault();
+          }}
+          value={inputData.email}
+          label="Email"
+          variant="outlined"
+          size="small"
+          style={{ width: 450 }}
+        />
+      </Box>
+      <Box display="flex" style={{ marginBlock: 10 }}>
+        <TextField
+          onChange={(event) => {
+            setInputData({ ...inputData, address: event.target.value });
+            event.preventDefault();
+          }}
+          value={inputData.address}
+          label="Alamat"
+          variant="outlined"
+          size="small"
+          style={{ width: 450 }}
+        />
+      </Box>
+      <Box display="flex" style={{ marginBlock: 10 }}>
+        <Button onClick={(event) => onSubmit(event)} variant="contained" color="primary">
+          Masukan Data
+        </Button>
+      </Box>
+    </Grid>
+  );
+}
+function InputForm() {
+  const { id } = useParams();
+  const [inputData, setInputData] = useState({
+    name: '',
+    phone_no: '',
+    email: '',
+    address: ''
+  });
+  const history = useHistory();
+  const classes = useStyles();
+
+  async function onSubmit(event) {
+    event.preventDefault();
+    axios
+      .post('http://localhost:8000/service/mechanic', {
+        id: uniqid('mechanic-'),
+        name: inputData.name,
+        phone_no: inputData.phone_no,
+        email: inputData.email,
+        address: inputData.address
       })
-    }
-  })
-  
+      .then((res) => {
+        if (res.status === 201) {
+          history.push({
+            pathname: '/mekanik',
+            state: 'Success'
+          });
+        } else {
+          history.push({
+            pathname: '/mekanik',
+            state: 'Failed'
+          });
+        }
+      });
+  }
+
+  return (
+    <Grid container direction="column" component={Paper} className={classes.boxInput}>
+      <Typography variant="h5" color="primary" style={{ marginBottom: 20 }}>
+        Buat PKB
+      </Typography>
+      <Box display="flex" style={{ marginBlock: 10 }}>
+        <TextField
+          onChange={(event) => {
+            setInputData({ ...inputData, name: event.target.value });
+            event.preventDefault();
+          }}
+          value={id}
+          label="Nama Mekanik"
+          variant="outlined"
+          size="small"
+          style={{ width: 450 }}
+        />
+      </Box>
+      <Box display="flex" style={{ marginBlock: 10 }}>
+        <TextField
+          onChange={(event) => {
+            setInputData({ ...inputData, phone_no: event.target.value });
+            event.preventDefault();
+          }}
+          value={inputData.phone_no}
+          label="Nomor Telepon"
+          variant="outlined"
+          size="small"
+          style={{ width: 450 }}
+        />
+      </Box>
+      <Box display="flex" style={{ marginBlock: 10 }}>
+        <TextField
+          onChange={(event) => {
+            setInputData({ ...inputData, email: event.target.value });
+            event.preventDefault();
+          }}
+          value={inputData.email}
+          label="Email"
+          variant="outlined"
+          size="small"
+          style={{ width: 450 }}
+        />
+      </Box>
+      <Box display="flex" style={{ marginBlock: 10 }}>
+        <TextField
+          onChange={(event) => {
+            setInputData({ ...inputData, address: event.target.value });
+            event.preventDefault();
+          }}
+          value={inputData.address}
+          label="Alamat"
+          variant="outlined"
+          size="small"
+          style={{ width: 450 }}
+        />
+      </Box>
+      <Box display="flex" style={{ marginBlock: 10 }}>
+        <Button onClick={(event) => onSubmit(event)} variant="contained" color="primary">
+          Masukan Data
+        </Button>
+      </Box>
+    </Grid>
+  );
 }
-
-return(
-  <Grid container direction="column" component={Paper} className={classes.boxInput}>
-    <Typography variant="h5" color="primary" style={{marginBottom:20}}>Input Service</Typography>
-    <Box display="flex" style={{marginBlock:10}}>
-      <TextField onChange={(event)=>{setInputData({...inputData, name: event.target.value}); event.preventDefault()}} value={id} label="Nama Mekanik" variant="outlined" size="small" style={{width:450}}/>
-    </Box>
-    <Box display="flex" style={{marginBlock:10}}>
-      <TextField onChange={(event)=>{setInputData({...inputData, phone_no: event.target.value}); event.preventDefault()}} value={inputData.phone_no} label="Nomor Telepon" variant="outlined" size="small" style={{width:450}}/>
-    </Box>
-    <Box display="flex" style={{marginBlock:10}}>
-      <TextField onChange={(event)=>{setInputData({...inputData, email: event.target.value}); event.preventDefault()}} value={inputData.email} label="Email" variant="outlined" size="small" style={{width:450}}/>
-    </Box>
-    <Box display="flex" style={{marginBlock:10}}>
-      <TextField onChange={(event)=>{setInputData({...inputData, address: event.target.value}); event.preventDefault()}} value={inputData.address} label="Alamat" variant="outlined" size="small" style={{width:450}}/>
-    </Box>
-    <Box display="flex" style={{marginBlock:10}}>
-      <Button onClick={(event)=>onSubmit(event)} variant="contained" color="primary">Masukan Data</Button>
-    </Box>
-  </Grid>
-)
-}
-function InputForm(props){
-  const {id} = useParams();
-
-const [open, setOpen] = useState(false);
-const [inputData, setInputData] = useState({
-  name:'',
-  phone_no:'',
-  email:'',
-  address:'',
-})
-const history = useHistory()
-const classes = useStyles()
-
-async function onSubmit(event){
-  event.preventDefault()
-  axios.post('http://localhost:8000/service/mechanic',{
-    id: uniqid("mechanic-"),
-    name: inputData.name,
-    phone_no: inputData.phone_no,
-    email: inputData.email,
-    address: inputData.address
-  }).then(res => {
-    if(res.status === 201){
-      history.push({
-        pathname: '/mekanik',
-        state: "Success"
-      })
-    }else{
-      history.push({
-        pathname: '/mekanik',
-        state: "Failed"
-      })
-    }
-  })
-  
-}
-
-return(
-  <Grid container direction="column" component={Paper} className={classes.boxInput}>
-    <Typography variant="h5" color="primary" style={{marginBottom:20}}>Buat PKB</Typography>
-    <Box display="flex" style={{marginBlock:10}}>
-      <TextField onChange={(event)=>{setInputData({...inputData, name: event.target.value}); event.preventDefault()}} value={id} label="Nama Mekanik" variant="outlined" size="small" style={{width:450}}/>
-    </Box>
-    <Box display="flex" style={{marginBlock:10}}>
-      <TextField onChange={(event)=>{setInputData({...inputData, phone_no: event.target.value}); event.preventDefault()}} value={inputData.phone_no} label="Nomor Telepon" variant="outlined" size="small" style={{width:450}}/>
-    </Box>
-    <Box display="flex" style={{marginBlock:10}}>
-      <TextField onChange={(event)=>{setInputData({...inputData, email: event.target.value}); event.preventDefault()}} value={inputData.email} label="Email" variant="outlined" size="small" style={{width:450}}/>
-    </Box>
-    <Box display="flex" style={{marginBlock:10}}>
-      <TextField onChange={(event)=>{setInputData({...inputData, address: event.target.value}); event.preventDefault()}} value={inputData.address} label="Alamat" variant="outlined" size="small" style={{width:450}}/>
-    </Box>
-    <Box display="flex" style={{marginBlock:10}}>
-      <Button onClick={(event)=>onSubmit(event)} variant="contained" color="primary">Masukan Data</Button>
-    </Box>
-  </Grid>
-)
-}
-
-
 
 function AddNewPKB() {
-    return(
-      <Box>
-        <InputForm/>
-        <InputServiceForm/>
-      </Box>
-    )
-  }
-  
+  return (
+    <Box>
+      <InputForm />
+      <InputServiceForm />
+    </Box>
+  );
+}
+
 export default AddNewPKB;
--- src/Pages/Printer/PrinterPKB.js
+++ src/Pages/Printer/PrinterPKB.js
@@ -1,56 +1,65 @@
-import { Typography, Box, Paper, Grid, Button, Modal, Fade, Backdrop, TextField, List, ListItem, ListItemText, InputAdornment  } from "@material-ui/core";
-import { makeStyles } from '@material-ui/core/styles';
-import { Search } from "@material-ui/icons";
-import { useEffect, useState } from "react";
-import { useHistory, useParams } from "react-router";
+/* eslint-disable react/prop-types */
+import { Typography, Box } from '@material-ui/core';
+// import { makeStyles } from '@material-ui/core/styles';
+// import { Search } from '@material-ui/icons';
+// import { useEffect, useState } from 'react';
+import { useParams } from 'react-router';
 
-const useStyles = makeStyles((theme)=>({
-  modal: {
-    display: 'flex',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  paper: {
-    backgroundColor: theme.palette.background.paper,
-    boxShadow: theme.shadows[5],
-    padding: theme.spacing(2, 4, 3),
-  },
-  boxInput:{
-    marginBlock:theme.spacing(3),
-    padding:theme.spacing(3),
-  }
-}));
+// const useStyles = makeStyles((theme) => ({
+//   modal: {
+//     display: 'flex',
+//     alignItems: 'center',
+//     justifyContent: 'center'
+//   },
+//   paper: {
+//     backgroundColor: theme.palette.background.paper,
+//     boxShadow: theme.shadows[5],
+//     padding: theme.spacing(2, 4, 3)
+//   },
+//   boxInput: {
+//     marginBlock: theme.spacing(3),
+//     padding: theme.spacing(3)
+//   }
+// }));
 
-const Iframe = (props) =>{
-  return(         
-    <div>          
-      <iframe id="iframe" src={props.src} height={props.height} width={props.width} frameborder="0"/>         
+const Iframe = (props) => {
+  return (
+    <div>
+      <iframe
+        id="iframe"
+        src={props.src}
+        height={props.height}
+        width={props.width}
+        frameBorder="0"
+      />
     </div>
-  )
+  );
 };
 
-function PrinterPKB(props) {
-    let { id } = useParams();
+function PrinterPKB() {
+  let { id } = useParams();
 
-    // const [master, setMaster] = useState({})
+  // const [master, setMaster] = useState({})
 
-    // useEffect(()=>{
+  // useEffect(()=>{
 
-    //   fetch("http://localhost:8000/staff/"+id)
-    //   .then(res =>res.json())
-    //   .then(
-    //     (result)=>{
-    //       setMaster(result)
-    //     }
-    //   )
-    // },[])
+  //   fetch("http://localhost:8000/staff/"+id)
+  //   .then(res =>res.json())
+  //   .then(
+  //     (result)=>{
+  //       setMaster(result)
+  //     }
+  //   )
+  // },[])
 
-    return(
-      <Box>
-        <Typography variant="h5" color="primary" style={{marginBottom:20}}>Printer PKB</Typography>
-        <Iframe src={`http://localhost:8000/printer?t=pkb&id=${id}`} height="1500" width="900"/>
-      </Box>
-    )
-  }
-  
+  return (
+    <Box>
+      <Typography variant="h5" color="primary" style={{ marginBottom: 20 }}>
+        Printer PKB
+      </Typography>
+      <Iframe src={`http://localhost:8000/printer?t=pkb&id=${id}`} height="1500" width="900" />
+    </Box>
+  );
+}
+
 export default PrinterPKB;
